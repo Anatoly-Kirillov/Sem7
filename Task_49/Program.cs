1f4db0757bd70c980e1e2c701e527143ab865bc4
Anatoly Kirillov <kirillovtolia@gmail.com>
2022-09-08 22:50:14 +0300

    Таск 48

---
+++ Task_49/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 49 сгенерировать массив и для элементов с обоими четными индексами произвети замену на квадрат имеющегося значения
+
+void FillArray(int[,] collection)
+{
+    int rows = 0;
+    int cols = 0;
+    for (rows = 0; rows < collection.GetLength(0); rows++)
+    {
+        for (cols = 0; cols < collection.GetLength(1); cols++)
+        {
+            collection[rows, cols] = new Random().Next(0, 10);
+        }
+
+    }
+}
+void PrintArray(int[,] collection1)
+{
+    int rows = 0;
+    int cols = 0;
+    for (rows = 0; rows < collection1.GetLength(0); rows++)
+    {
+        for (cols = 0; cols < collection1.GetLength(1); cols++)
+        {
+            Console.Write($"  {collection1[rows, cols]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void ArrayEvenInPower(int[,] collection2)
+{
+    int rows = 0;
+    int cols = 0;
+    for (rows = 0; rows < collection2.GetLength(0); rows++)
+    {
+        for (cols = 0; cols < collection2.GetLength(1); cols++)
+        {
+            if (rows % 2 == 0 && cols % 2 == 0)
+                void ArrayEvenInPower(int[,] collection2)
+        }
+    }    
+}
+// void ChangeMatrix(int[,] array)
+//{
+//    for (int i = 0; i < array.GetLength(0); i++)
+//    {
+//        if (i % 2 == 0)
+//        {
+//            for (int j = 0; j < array.GetLength(1); j++)
+//            {
+//                if (j % 2 == 0)
+//                {
+//                    int pow = array[i, j];
+//                    array[i, j] = pow * pow;
+//                }
+//            }
+//        }
+//    }
+//}
+
+
+
+
---
+++ Task_51/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте двумерный массив.Найдите сумму элементов, 
+//находящихся на главной диагонали(с индексами(0,0);(1,1) и т.д.)
+void diagonalArrayElementsSumm (int[,] collection2)
+{
+    int diagonalPosition = 0;
+    int Summ = 0; 
+    for (diagonalPosition = 0; diagonalPosition < collection2.GetLength(0); diagonalPosition++)
+    {
+        {
+           Summ = Summ + collection2[diagonalPosition, diagonalPosition];
+        }   
+    }
+    Console.WriteLine ($"Сумма диагональных элементов массива равна {Summ}");
+}
